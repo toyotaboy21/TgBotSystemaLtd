d38f1d7e7df873947f8a9d36dc5ff4b09fd5525b
Astamur Ladariya <149709871+reques6e@users.noreply.github.com>
2024-05-04 11:53:34 +0300

    Create installer.bash

---
+++ installer.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+chmod +x main.py
+sudo apt install python3-pip
+pip3 install -r assets/requirements.txt
+
+cat > /etc/systemd/system/tg_bot_systema.service <<EOF
+[Unit]
+Description=Flask App
+After=network.target
+
+[Service]
+User=root
+WorkingDirectory=$(pwd)
+ExecStart=$(which python) main.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable tg_bot_systema
+systemctl start tg_bot_systema
+
+echo "Установка завершена."
