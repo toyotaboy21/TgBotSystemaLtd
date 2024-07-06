a3707cacd2cbdfd14c107dfe10cb1b381e4794fc
Reques6e <voronin99borja61275y@rambler.ru>
2024-07-06 09:28:37 +0300

    Импортирую новые файлы

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from aiogram import executor
 from bot.bot import dp
-from bot.handlers.commands import on_startup_commands
+from bot.handlers import user
+from bot.handlers import admin 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup_commands, skip_updates=True)
+    executor.start_polling(dp, on_startup=user.on_startup_commands, skip_updates=True)
